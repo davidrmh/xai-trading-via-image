e15d65564334fd391db029d6605d060cacef61a9
davidrmh <davidricardo888@hotmail.com>
2023-02-28 19:53:14 +0100

    Add functions to compute MACD signals

--- labels.py
+++ labels.py
@@ -34,3 +34,30 @@ def bollinger_signals(data: pd.DataFrame, window: int = 20, window_dev: Optional
     data = bollinger_buy(data, window)
     data = bollinger_sell(data, window)
     return data
+
+def macd_buy(data: pd.DataFrame, w_slow, w_sig) -> pd.DataFrame:
+    nobs = data.shape[0]
+    signal = [0.0] * nobs
+    for t in range(w_sig + w_slow - 1, nobs):
+        if (data['MACD'].iloc[t] > data['MACD_Sig'].iloc[t]) and (data['MACD'].iloc[t - 1] < data['MACD_Sig'].iloc[t - 1]):
+            signal[t] = 1.0
+    data['MACD_Buy'] = signal
+    return data
+
+def macd_sell(data: pd.DataFrame, w_slow, w_sig) -> pd.DataFrame:
+    nobs = data.shape[0]
+    signal = [0.0] * nobs
+    for t in range(w_sig + w_slow - 1, nobs):
+        if (data['MACD'].iloc[t] < data['MACD_Sig'].iloc[t]) and (data['MACD'].iloc[t - 1] > data['MACD_Sig'].iloc[t - 1]):
+            signal[t] = 1.0
+    data['MACD_Sell'] = signal
+    return data
+
+def macd_signals(data: pd.DataFrame, w_slow: int = 26, w_fast: int = 12, w_sig: int = 9) -> pd.DataFrame:
+    macd = MACD(data['Adj Close'], window_slow = w_slow, window_fast = w_fast, window_sign = w_sig)
+    data['MACD'] = macd.macd()
+    data['MACD_Sig'] = macd.macd_signal()
+    data = macd_buy(data, w_slow, w_sig)
+    data = macd_sell(data, w_slow, w_sig)
+    return data
+    
