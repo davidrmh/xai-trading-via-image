49c1d9e29ba7829127a67db33b962c949a235ded
davidrmh <davidricardo888@hotmail.com>
2023-02-28 20:07:19 +0100

    Fix bug in bollinger_signals function

--- labels.py
+++ labels.py
@@ -10,7 +10,7 @@ def bollinger_buy(data: pd.DataFrame, window: int) -> pd.DataFrame:
     for t in range(window - 1, nobs):
         if (data['Adj Close'].iloc[t - 1] < data['Low_Band'].iloc[t - 1]) and (data['Adj Close'].iloc[t] > data['Low_Band'].iloc[t]):
             signal[t] = 1.0
-    data['Buy_Signal'] = signal
+    data['BB_Buy'] = signal
     return data
 
 def bollinger_sell(data: pd.DataFrame, window: int) -> pd.DataFrame:
@@ -19,14 +19,12 @@ def bollinger_sell(data: pd.DataFrame, window: int) -> pd.DataFrame:
     for t in range(window - 1, nobs):
         if (data['Adj Close'].iloc[t - 1] > data['Up_Band'].iloc[t - 1]) and (data['Adj Close'].iloc[t] < data['Up_Band'].iloc[t]):
             signal[t] = 1.0
-    data['Sell_Signal'] = signal
+    data['BB_Sell'] = signal
     return data
     
-def bollinger_signals(data: pd.DataFrame, window: int = 20, window_dev: Optional[int] = None) -> pd.DataFrame:
-    window_dev = window if window_dev == None else window_dev
-    
+def bollinger_signals(data: pd.DataFrame, window: int = 20, n_std: int = 2) -> pd.DataFrame:
     # Compute Bollinger bands
-    bollinger = BollingerBands(data['Adj Close'], window, window_dev)
+    bollinger = BollingerBands(data['Adj Close'], window, n_std)
     low_band = bollinger.bollinger_lband()
     up_band = bollinger.bollinger_hband()
     data['Low_Band'] = low_band
