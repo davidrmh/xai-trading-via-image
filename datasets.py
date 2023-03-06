3e01e64bbe9f09de1acef0f4e7e3402059f79391
davidrmh <davidricardo888@hotmail.com>
2023-03-06 15:02:51 +0100

    Remove bool_norm parameter

--- datasets.py
+++ datasets.py
@@ -5,22 +5,20 @@ import torch
 
 class ImageDataset(Dataset):
     
-    def __init__(self, dir_pos: str, dir_neg: str, bool_norm: bool = True):
+    def __init__(self, dir_pos: str, dir_neg: str):
         self.dir_pos = dir_pos
         self.dir_neg = dir_neg
-        self.bool_norm = bool_norm
         self.files = os.listdir(dir_pos) + os.listdir(dir_neg)
         self.labels = [1.0] * int(len(self.files) / 2) + [0.0] * int(len(self.files) / 2)
         self.labels = torch.tensor(self.labels, dtype = torch.float32)
     
-    def image2tensor(self, image_path, bool_norm = True) -> torch.Tensor:
+    def image2tensor(self, image_path) -> torch.Tensor:
         image = read_image(image_path, ImageReadMode.RGB)
         #Change dtype to float32
         image = image.to(torch.float32)
 
         # Normalize in [0, 1]
-        if bool_norm:
-            image = image / image.max()
+        image = image / image.max()
 
         return image
     
@@ -32,7 +30,7 @@ class ImageDataset(Dataset):
             image_path = os.path.join(self.dir_pos, self.files[idx])
         else:
             image_path = os.path.join(self.dir_neg, self.files[idx])
-        image = self.image2tensor(image_path, self.bool_norm)
+        image = self.image2tensor(image_path)
         
         return image, self.labels[idx]
         
