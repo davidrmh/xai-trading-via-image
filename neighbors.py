a27a591a8e8377b13a3f3fa662b1de270d322660
davidrmh <davidricardo888@hotmail.com>
2023-03-10 19:50:14 +0100

    Backup still working

--- neighbors.py
+++ neighbors.py
@@ -6,6 +6,10 @@ from utils import image2tensor
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 
+# Labels for positive/negative class
+lab_pos = 'Buy'
+lab_neg = 'No Buy'
+
 # File containing the predictions from a classifier over a
 # test set
 pred_file = "./70_by_70_predictions_images_2018/pred_BB_Buy_classif.csv"
@@ -39,6 +43,8 @@ for i, idx in enumerate(wrong_idx):
     # Get query image
     path_img_query = df_pred.iloc[idx, 0]
     str_aux_query = df_pred.iloc[idx, 0].split('/')[-1]
+    pred_query = lab_pos if df_pred.iloc[idx]['prediction'] == 1 else lab_neg
+    true_query = lab_pos if df_pred.iloc[idx]['truth'] == 1 else lab_neg
     
     # TO DO: There must be an easier way of doing this
     for j, f in zip(latent_test['file'].index, latent_test['file']):
@@ -58,9 +64,22 @@ for i, idx in enumerate(wrong_idx):
     
     # Plot row
     for j, idx_n in enumerate(idx_neighbors):
+        
+        # Find labels for neighbor
+        str_aux_neighbor = latent_train.iloc[idx_n]['file']
+        str_aux_neighbor = str_aux_neighbor.split('\\')[-1]
+        
+        # I need the DataFrame with predictions from the training data
+        for k, f_str in enumerate(df_pred['file']):
+            if str_aux_neighbor in f_str:
+                pred_neighbor = lab_pos if df_pred.iloc[k]['prediction'] == 1 else lab_neg
+                true_neighbor = lab_pos if df_pred.iloc[k]['truth'] == 1 else lab_neg
+                
+        
         # Plot query image
         if j == 0:
             axs[i, j].imshow(img_query)
+            
         # Plot neighbor image
         else:
             img_neighbor = image2tensor(latent_train['file'][idx_n])
@@ -70,13 +89,4 @@ for i, idx in enumerate(wrong_idx):
         axs[i,j].set_yticks([])
 plt.subplots_adjust(bottom = 0.1, top = 0.5)
 plt.show()
-        
-    
-    
-    
-    
-    
-    
-    
-
 
