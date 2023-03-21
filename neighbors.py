a9bf3843c4ad627be1f3bf04d4f1a1063be017eb
davidrmh <davidricardo888@hotmail.com>
2023-03-21 18:19:58 +0100

    Backup. Working and changing a lot of stuff that probably I'm not gonna keep

--- neighbors.py
+++ neighbors.py
@@ -19,13 +19,13 @@ from sklearn.neighbors import NearestNeighbors
 train_pred_file = "./70_by_70_predictions_images_2010_2017/pred_BB_Buy_classif.csv"
 
 # File containing the predictions from a classifier over the test set
-test_pred_file = "./70_by_70_predictions_images_2018/pred_BB_Buy_classif.csv"
+test_pred_file = "./70_by_70_predictions_images_2010_2017/pred_BB_Buy_classif.csv"
 
 # File containing the latent representations of the images in the train set
 train_lat_file = "./70_by_70_pca/train_pca_BB_Buy_autoencoder.pkl"
 
 # File containing the latent representations of the images in the test set
-test_lat_file = "./70_by_70_pca/test_pca_BB_Buy_autoencoder.pkl"
+test_lat_file = "./70_by_70_pca/train_pca_BB_Buy_autoencoder.pkl"
 
 # File containing the path of a trained autoencoder
 path_autoencoder = './70_by_70_autoencoders/BB_Buy_autoencoder.pth'
@@ -46,39 +46,39 @@ bool_type = False
 num_neighbors = 5
 
 # Output Figure size
-figsize = (7, 7)
+figsize = (10, 10)
 
 # DPI
-dpi = 600
+dpi = 300
 
-# Image size
+# Input image size
 image_size = (70, 70)
 
 # Mask size
 mask_size = 10 # Need to think how to select this parameter
 
 # stride size
-stride = 3 # Need to think how to select this parameter
+stride = 4 # Need to think how to select this parameter
 
 # Labels for positive/negative class
 lab_pos = 'Buy'
 lab_neg = 'No Buy'
 
 # Colormaps
-sim_col_map = 'BuGn'
+sim_col_map = 'cool'
 dis_col_map = 'YlOrRd'
 
 # Transparency
-alpha = 0.4
+alpha = 0.9
 
 # Path where to store the results
 out_path = './70_by_70_sd_maps'
 
 # name of the file with the output
-out_file = 'sd_map8'
+out_file = 'sd_map4'
 
 # For reproducibility
-seed = 86
+seed = 4
 # ---------------------END OF CONFIG FILE ------------------------
 
 
@@ -89,7 +89,7 @@ def overall_level(list_maps: list) -> float:
     
     # Aggregate (dis)similarity maps
     for m in list_maps:
-        sum_map += m
+        sum_map = m + sum_map
     
     # Normalize to [0, 1]
     sum_map /= sum_map.max()
@@ -99,8 +99,8 @@ def overall_level(list_maps: list) -> float:
 def mask_image(source_img: torch.Tensor, mask_size: int, stride: int):
 
     image_size = source_img.shape[1:]
-    non_normed_masks = torch.zeros(image_size) # Change name to norm_factor
-    masked_neighbor_image = torch.Tensor() # Change name to masked_images
+    factor_f = torch.zeros(image_size)
+    masked_neighbor_image = torch.Tensor()
     indices = []
     for i in range(0, image_size[0] - mask_size + 1, stride):
         for j in range(0, image_size[1] - mask_size + 1, stride):
@@ -112,9 +112,9 @@ def mask_image(source_img: torch.Tensor, mask_size: int, stride: int):
                 # [(start_row, end_row), (start_col, end_col)]
                 indices.append([(i, min(i + mask_size, image_size[0])), (j,min(j + mask_size, image_size[1]))])
                 masked_neighbor_image = torch.cat((masked_neighbor_image, (source_img * mask).unsqueeze(0)), 0)
-                non_normed_masks += 1 - mask # invert to get area that is masked, e.g. from [1, 1, 0, 1, 1] to [0, 0, 1, 0, 0]
+                factor_f[i:min(i + mask_size, image_size[0]), j:min(j + mask_size, image_size[1])] += 1
 
-    return masked_neighbor_image, non_normed_masks, indices
+    return masked_neighbor_image, factor_f, indices
 
 def get_latent_masks(model: resnet.ResNetAutoEncoder,
                      masked_neighbor_image: torch.Tensor,
@@ -141,6 +141,40 @@ def get_latent_masks(model: resnet.ResNetAutoEncoder,
 
     return f_maps
 
+def get_regions(unmask_dist: float,
+                masked_distances: np.ndarray,
+                idx_mask: list):
+    sim_regions = []
+    dis_regions = []
+    sim_contrib = []
+    dis_contrib = []
+    
+    # Find similarity regions
+    for i, d in enumerate(masked_distances):
+        
+        # Similarity region
+        if d >= unmask_dist:
+            sim_regions.append(idx_mask[i])
+            sim_contrib.append(d)
+        
+        # Dissimilarity region
+        else:
+            dis_regions.append(idx_mask[i])
+            dis_contrib.append(d)
+    
+    # Compute contributions
+    if len(sim_contrib) > 0:
+        sim_contrib = np.array(sim_contrib)
+        range_sim = sim_contrib.max() - sim_contrib.min()
+        sim_contrib = (sim_contrib - sim_contrib.min()) / range_sim
+
+    if len(dis_contrib) > 0:
+        dis_contrib = np.array(dis_contrib)
+        range_dis = dis_contrib.max() - dis_contrib.min()
+        dis_contrib = (dis_contrib - dis_contrib.min()) / range_dis
+    
+    return sim_regions, sim_contrib, dis_regions, dis_contrib
+
 def get_sd_map(img_neighbor: torch.Tensor,
                latent_query: np.ndarray,
                unmask_dist: float,
@@ -153,7 +187,7 @@ def get_sd_map(img_neighbor: torch.Tensor,
     image_size = img_neighbor.shape[1:]
     
     # Make masked images
-    masked_image, norm_factor, idx_mask = mask_image(img_neighbor, mask_size, stride)
+    masked_image, factor_f, idx_mask = mask_image(img_neighbor, mask_size, stride)
     
     # Get latent representations of each masked image
     latent_masks = get_latent_masks(model, masked_image, pca, scaler)
@@ -163,46 +197,47 @@ def get_sd_map(img_neighbor: torch.Tensor,
     # of masked versions of the neighbor image
     masked_distances = cdist(latent_query[np.newaxis, :], latent_masks, metric = 'euclidean')[0]
     
-    # Compute importance of masked regions
-    sim_importance_masked_regions = [max(mask_dist - unmask_dist, 0) for mask_dist in masked_distances]
-    dissim_importance_masked_regions = [max(unmask_dist - mask_dist, 0) for mask_dist in masked_distances]
-    
-    # Similarity
-    min_imp = min(sim_importance_masked_regions)
-    max_imp = max(sim_importance_masked_regions)
-    scaled_sim_importance_masked_regions = np.array([(f-min_imp) / (max_imp - min_imp)  for f in sim_importance_masked_regions])
-    
-    # Dissimilarity
-    min_imp = min(dissim_importance_masked_regions)
-    max_imp = max(dissim_importance_masked_regions)
-    scaled_dissim_importance_masked_regions = np.array([(f-min_imp) / (max_imp - min_imp)  for f in dissim_importance_masked_regions])
-    
-    # The normalization factors considers the number of times
-    # a region is overlapped by a mask
-    norm = np.exp(-norm_factor/norm_factor.max())
-    #norm = 1 / norm_factor
-    
+    # Obtain (dis)similarity regions and their contributions
+    sim_regions, sim_contrib, dis_regions, dis_contrib = get_regions(unmask_dist, masked_distances, idx_mask)
     
     # Saliency Maps
-    sim_based_sm = np.zeros(image_size)
-    dissim_based_sm = np.zeros(image_size)
+    sim_map = torch.zeros_like(img_neighbor)
+    dis_map = np.zeros(image_size)
     
-    for i, e in enumerate(idx_mask):
-        mask = np.zeros(image_size)
+    # Similarity map
+    for i, e in enumerate(sim_regions):
         start_row = e[0][0]
         end_row = e[0][1]
         start_col = e[1][0]
         end_col = e[1][1]
-        mask[start_row:end_row, start_col:end_col] = norm[start_row:end_row, start_col:end_col]
-        #mask[start_row:end_row, start_col:end_col] = 1.0
-        sim_based_sm += mask * scaled_sim_importance_masked_regions[i]
-        dissim_based_sm +=  mask * scaled_dissim_importance_masked_regions[i]
+        
+        # aux is used to only consider part of img_neighbor that is not background
+        img_neighbor_region = img_neighbor[:, start_row:end_row, start_col:end_col]
+        aux = torch.zeros_like(img_neighbor_region)
+        aux[img_neighbor_region > 0] = 1.0
+        
+        # Factor to consider overlapping masks
+        # factor = np.exp(- factor_f[start_row:end_row, start_col:end_col] / factor_f[start_row:end_row, start_col:end_col].max())
+        factor = 1 / factor_f[start_row:end_row, start_col:end_col]
+        factor = factor.reshape((1, *factor.shape))
+        sim_map[:, start_row:end_row, start_col:end_col] = sim_contrib[i] * factor * aux + sim_map[:, start_row:end_row, start_col:end_col]
+    #sim_map = sim_map.sum(axis = 0).numpy()
+    sim_map[0][sim_map[0] > 0] = 1
+    sim_map[1][sim_map[1] > 0] = 1
+    sim_map[2] = 0
+    sim_map = sim_map.permute([1,2,0]).numpy()
+
+    # Dissimilarity map
+    for i, e in enumerate(dis_regions):
+        start_row = e[0][0]
+        end_row = e[0][1]
+        start_col = e[1][0]
+        end_col = e[1][1]
+        #factor = np.exp(- factor_f[start_row:end_row, start_col:end_col] / factor_f[start_row:end_row, start_col:end_col].max())
+        factor = 1 / factor_f[start_row:end_row, start_col:end_col]
+        dis_map[start_row:end_row, start_col:end_col] = dis_contrib[i] * factor + dis_map[start_row:end_row, start_col:end_col]
     
-    # Normalize to [0, 1]
-    sim_based_sm /= sim_based_sm.max()
-    dissim_based_sm /= dissim_based_sm.max()
-    
-    return sim_based_sm, dissim_based_sm
+    return sim_map, dis_map
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -238,13 +273,19 @@ query_idx = np.random.choice(df_test_pred[df_test_pred['is_correct?'] == bool_ty
                              size = num_query,
                              replace = False)
 
-# Fit nearest neighbors object
-knn = NearestNeighbors(n_neighbors = num_neighbors)
-knn.fit(latent_train.iloc[:, 1:].to_numpy())
-
-# To control plots
 plt.ioff()
 fig, axs = plt.subplots(2 * num_query, num_neighbors + 1, figsize = figsize)
+
+# This if is used to make the analysis over the training set
+# When the test set is set equal to the training set we have
+# to discard the query image as neighbor (distance 0)
+if train_lat_file == test_lat_file:
+    knn = NearestNeighbors(n_neighbors = num_neighbors + 1)
+else:
+    knn = NearestNeighbors(n_neighbors = num_neighbors)
+    
+# Fit nearest neighbors object
+knn.fit(latent_train.iloc[:, 1:].to_numpy())
 
 
 # Color maps
@@ -273,12 +314,15 @@ for idx in query_idx:
             break
     img_query = image2tensor(path_img_query)
 
-    # Get neighbors and orders them (increasingly) according to their distance
-    # to the latent representation of the query image
+    # Get neighbors of the latent representation of the query image
     dist, idx_neighbors = knn.kneighbors(latent_query.reshape((1, -1)), return_distance = True)
     dist = dist.reshape((dist.shape[1], ))
     idx_neighbors = idx_neighbors.reshape((idx_neighbors.shape[1], ))
-    idx_neighbors = idx_neighbors[dist.argsort()[::-1]]
+    # This if is used to make the analysis over the training set
+    # When the test set is set equal to the training set we have
+    # to discard the query image as neighbor (distance 0)
+    if train_lat_file == test_lat_file:
+        idx_neighbors = idx_neighbors[1:]
     
     # To aggregate (dis)similarities of neighbors
     sim_agg = torch.zeros(image_size)
@@ -297,9 +341,12 @@ for idx in query_idx:
     ola_dif = 0.0 # Overall level of agreement for neighbors with different predicted label as query image
     old_dif = 0.0 # Overall level of disagreement for neighbors with different predicted label as query image
     
+    # For debbuging
+    print(f' {"=" * 20} Query image {idx} {"=" * 20}')
 
     # Plot row
     for j, idx_n in enumerate(idx_neighbors):
+        print(f' {"=" * 20} Neighbor image {idx_n} {"=" * 20}')
         
         # Find labels for neighbor
         str_aux_neighbor = latent_train.iloc[idx_n]['file']
@@ -336,12 +383,12 @@ for idx in query_idx:
         
         # Make plots for neighbors
         sim_map = torch.from_numpy(sim_map)
-        sim_agg += sim_map
-        sim_map[sim_map <= sim_map[sim_map > 0].quantile(0.1)] = torch.nan
+        #sim_agg += sim_map
+        sim_map[sim_map == 0] = torch.nan
         
         dis_map = torch.from_numpy(dis_map)
         dis_agg += dis_map
-        dis_map[dis_map <= dis_map[dis_map > 0].quantile(0.1)] = torch.nan
+        dis_map[dis_map == 0.0] = torch.nan
 
         # Plot neighbor image with similarity map
         img_neighbor = img_neighbor.permute([1, 2, 0])
@@ -353,7 +400,9 @@ for idx in query_idx:
         
         # Plot neighbor image with dissimilarity map
         axs[i + 1, j + 1].imshow(img_neighbor)
-        axs[i + 1, j + 1].imshow(dis_map, alpha = alpha, cmap = cmap_dis)
+        axs[i + 1, j + 1].imshow(dis_map, alpha = alpha, cmap = cmap_dis,
+                                vmin = np.nanmin(dis_map),
+                                vmax = np.nanmax(dis_map))
         axs[i + 1, j + 1].set_title(f'Pred: {pred_neighbor}\n True: {true_neighbor}', fontsize=5)
         axs[i + 1, j + 1].set_xticks([])
         axs[i + 1, j + 1].set_yticks([])
@@ -365,21 +414,25 @@ for idx in query_idx:
     old_dif = overall_level(list_dis_dif)
     
     # Plot query image with aggregated similarity maps
-    sim_agg /= sim_agg.max()
-    sim_agg[sim_agg <= sim_agg[sim_agg > 0].quantile(0.1)] = torch.nan
+    #sim_agg /= sim_agg.max()
+    #sim_agg[sim_agg == 0.0] = torch.nan
     img_query = img_query.permute([1, 2, 0])
     axs[i, 0].imshow(img_query)
-    axs[i, 0].imshow(sim_agg, alpha = alpha, cmap = cmap_sim)
+    #axs[i, 0].imshow(sim_agg / num_neighbors, alpha = alpha, cmap = cmap_sim,
+    #                vmin = np.nanmin(sim_agg / num_neighbors),
+    #                vmax = np.nanmax(sim_agg / num_neighbors))
     axs[i, 0].set_title(f'Pred: {pred_query}\n True: {true_query}', fontsize=5)
     axs[i, 0].set_xlabel(f'OLA_D:{ola_dif:.4f} \n OLA_S: {ola_same:.4f}', fontsize=5)
     axs[i, 0].set_xticks([])
     axs[i, 0].set_yticks([])
     
     # Plot query image with aggregated dissimilarity maps
-    dis_agg /= dis_agg.max()
-    dis_agg[dis_agg <= dis_agg[dis_agg > 0].quantile(0.1)] = torch.nan
+    #dis_agg /= dis_agg.max()
+    dis_agg[dis_agg == 0.0] = torch.nan
     axs[i + 1, 0].imshow(img_query)
-    axs[i + 1, 0].imshow(dis_agg, alpha = alpha, cmap = cmap_dis)
+    #axs[i + 1, 0].imshow(dis_agg / num_neighbors, alpha = alpha, cmap = cmap_dis,
+    #                    vmin = np.nanmin(dis_agg / num_neighbors),
+    #                    vmax = np.nanmin(dis_agg / num_neighbors))
     axs[i + 1, 0].set_title(f'Pred: {pred_query}\n True: {true_query}', fontsize = 5)
     axs[i + 1, 0].set_xlabel(f'OLD_D: {old_dif:.4f} \n OLD_S: {old_same:.4f}', fontsize = 5)
     axs[i + 1, 0].set_xticks([])
